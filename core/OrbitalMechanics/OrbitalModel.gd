bff18367f193fb3cc3f5aa679ad453ee4d07b1ed
cj05 <cjcoolsuper@gmail.com>
2026-01-12 12:32:55 +0700

    fix: Gitignore added

--- core/OrbitalMechanics/OrbitalModel.gd
+++ core/OrbitalMechanics/OrbitalModel.gd
@@ -1,15 +1,29 @@
 class_name OrbitalModel
 
-var celestial_bodies: Dictionary[Node,AbstractBinding]
-# thats it hehe
+var celestial_bodies: Dictionary = {}
+
+# --- Cache variables ---
+var _cached_values: Array = []
+var _cache_dirty: bool = true
+
 func insert(object: AbstractBinding) -> void:
 	celestial_bodies[object.get_body()] = object
+	_cache_dirty = true
 	
 func delete(object: AbstractBinding) -> void:
-	celestial_bodies.erase(object.get_body())
+	if celestial_bodies.erase(object.get_body()):
+		_cache_dirty = true
 
 func get_by_node(node: Node) -> AbstractBinding:
-	return celestial_bodies[node]
+	return celestial_bodies.get(node)
 
-func values() -> Array[AbstractBinding]:
-	return celestial_bodies.values()
+func values() -> Array:
+	if _cache_dirty:
+		_update_cache()
+	return _cached_values
+
+func _update_cache() -> void:
+	_cached_values = celestial_bodies.values()
+	# Sort by mass descending: Parents first, then children
+	_cached_values.sort_custom(func(a, b): return a.mass > b.mass)
+	_cache_dirty = false
